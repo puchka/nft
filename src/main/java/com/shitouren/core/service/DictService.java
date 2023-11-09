c63e12ae5fe1dfed061534dac6c42110178b6b95
Marius Rabenarivo <mariusrabenarivo@gmail.com>
2023-11-09 13:14:18 +0400

    Remove everything but AddressUtils.java and Bech32Utils.java

--- src/main/java/com/shitouren/core/service/DictService.java
+++
@@ -1,8 +0,0 @@
-package com.shitouren.core.service;
-
-public interface DictService {
-    String getValue(String code);
-
-    void setValue(String code, String value);
-
-}
--- src/main/java/com/shitouren/core/service/WXAppPayService.java
+++
@@ -1,9 +0,0 @@
-package com.shitouren.core.service;
-
-import java.util.Map;
-
-public interface WXAppPayService {
-    Map<String, String> dounifiedOrder(String type, String out_trade_no, String total_fee) throws Exception;
-
-    String payBack(String resXml);
-}
